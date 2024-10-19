7b5dad28e0123d121c7e9e9a3c46d1c98ac34c1f
Abhishek Kaplesh <abkaplesh02@gmail.com>
2024-10-19 15:44:34 +0530

    update files

---
+++ Project 1/app.js
@@ -0,0 +1,14 @@
+// Hello world using console
+console.log("Hello World");
+
+
+// Hello world using alert()
+
+document.getElementById("Hello").addEventListener("click",function(){
+    alert("Hello World")
+});
+
+// Hello World using document.write();
+document.clear();
+
+document.write("<h2>Hello World!</h2><p>Have a nice day!</p>");
